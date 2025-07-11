14017ba9ef691f7a02ab4db7c995bca17177c212
deep <pardeep.kumar@adcube.ai>
2025-07-11 16:36:21 +0200

    update

--- fe/src/environments/environment.local.ts
+++ fe/src/environments/environment.local.ts
@@ -2,7 +2,7 @@ export const environment = {
   maintenance: false,
   production: false,
   envName: 'LOCAL',
-  apiHost: 'http://localhost:5050/',
+  apiHost: 'http://localhost:8000/',
   awsAuthRegion: 'eu-west-3',
   awsAuthUserPoolId: 'eu-west-3_LQi9V6Jy5',
   awsAuthUserPoolWebClientId: '149lm2j475rkp5ur27jbqri9vj'
